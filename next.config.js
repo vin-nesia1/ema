adb1c262d572d15de71fe94c2274c11ae06f8ec8
vin-nesia1 <vinnesia1.id@gmail.com>
2025-07-30 05:18:19 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-};
+  swcMinify: true,
+  // Pastikan API routes tidak di-prerender
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
+}
+
+module.exports = nextConfig
